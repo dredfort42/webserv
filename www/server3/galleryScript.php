3d572c5eb2c2f60a2722aaac0acb879a99871053
Kira Slepchenkova <rarakira@Kiras-MacBook-Pro-2.local>
2022-09-10 14:56:14 +0300

    Finished gallery with upload and delete functionality

--- www/server3/galleryScript.php
+++ www/server3/galleryScript.php
@@ -1,26 +1,24 @@
 <html>
     <head>
         <link rel="stylesheet" href="style.css" />
-
     </head>
     <body>
 
         <div class="container">
             <div class="hero">
-                <!-- <h1>A-ha-ha</h1> -->
                 <h2 style="text-align:center"><a href="./index.php"><- Back to the gallery</a></h2>
                 <?php
                     $image = $_GET['picture'];
                     if (file_exists($image)) {
                         echo "<img class='bigPic' src='".$image."'>";
+                        echo "<form method='post'>";
+                        echo "<button type='submit' name='deletebutton' class='red'>DELETE</button>";
+                        echo "</form>";
                     } else {
                         echo '<h2 style="text-align:center">The image was deleted successfully!</h2>';
                         header("Refresh:2; url=index.php");
                     }
-                ?>
 
-
-                <?php
                     if(isset($_POST['deletebutton'])) {
                         if (unlink($image)) {
                             header("Refresh:0");
@@ -29,13 +27,6 @@
                         }
                     }
                 ?>
-                
-                <form method="post">
-                    <button type="submit" name="deletebutton" class="red">
-                        DELETE
-                    </button>
-                </form>
-
             </div>
 		</div>
     </body>
