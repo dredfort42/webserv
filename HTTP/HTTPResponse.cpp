280096c6c0aad650f742de03b3d9edb3db5c3f48
Gilliam6 <wizzzardoz@icloud.com>
2022-09-15 23:01:14 +0400

    delete branch init

--- HTTP/HTTPResponse.cpp
+++ HTTP/HTTPResponse.cpp
@@ -47,6 +47,8 @@ std::string ws::HTTPResponse::DELETE(ws::HTTPreq &req, ws::Connection &connectio
 		}
 		path = connection.config.root + connection.config.uploadPath;
 	}
+	if (req.path.find(path) == std::string::npos)
+		return addHeader(response, req, "204");
 	path += std::string(req.path.substr(req.path.rfind("/") + 1));
 	std::cout << path;
 	ws::File myFd(path, OPEN_FILE);
@@ -55,7 +57,7 @@ std::string ws::HTTPResponse::DELETE(ws::HTTPreq &req, ws::Connection &connectio
 //	std::vector<uint8_t> tmp = myFd.readFile();
 //	response = std::string(tmp.begin(), tmp.end()); 
 	myFd.removeFile();
-	return addHeader(response, req, "200");
+	return addHeader(response, req, "202");
 
 }
 
